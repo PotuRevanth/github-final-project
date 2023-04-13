2beaf13b4b22bde0571864ad7b7e24efe9d1a517
PotuRevanth <98310054+PotuRevanth@users.noreply.github.com>
2023-04-13 21:26:18 +0530

    updated readme
    updated readme and comments for code.

--- simple-interest.sh
+++ simple-interest.sh
@@ -10,12 +10,12 @@
    # t, time period in years
    # r, annual rate of interest
    # Output:
-   # simple interest = p*t*r
-   echo "Enter the principal:"
+   # simple interest = p*t*r/100
+   echo "Enter the principal:"  #enter amount
    read p
-   echo "Enter rate of interest per year:"
+   echo "Enter rate of interest per year:" #interest rate
    read r
-   echo "Enter time period in years:"
+   echo "Enter time period in years:" #time from given date in years
    read t
    s=`expr $p \* $t \* $r / 100`
    echo "The simple interest is: "
